486bb3332a0e286d9dda6b1e0d1ad9d91122459b
Xiahua Liu <liuxiafa@gmail.com>
2019-12-04 11:21:49 -0500

    Dec 3

--- cube.py
+++ cube.py
@@ -1,24 +1,92 @@
 import numpy as np 
 
-class Cube222:
+class cube222:
     ## Cube has information on each side
 
     # Color is one-hot encoded (binary one-hot encoding)
-    # White  : 1
-    # Yellow : 10
-    # Orange : 100
-    # Red    : 1000
-    # Green  : 10000
-    # Blue   : 100000
+    # White   : 1
+    # Orange  : 10
+    # Green   : 100
+    # Red     : 1000
+    # Blue    : 10000
+    # Yellow  : 100000
         
     def __init__(self, scram_num):
-        self.pattern=np.zeros([6,2,2])
-        face_order=np.random.permutation(6)
-        for i in range(6)
-            self.pattern[i]=np.full([2,2],1<<face_order[i])
-        for i in range(scram_num+1)
+        self.pattern=np.zeros([6,2,2],dtype=np.uint32)
+        self.color=('W','O','G','R','B','Y')
+        self.face=("U","L","F","R","B","D")
+        for i in range(6):
+            self.pattern[i]=np.full([2,2],i,dtype=np.uint32)
+        for i in range(scram_num):
             self.turn(np.random.permutation(6)[0]) 
             # Turn a random face
         
     def turn(self, face):
+        if face==0:
+            self.pattern[0]=np.array(list(zip(*self.pattern[0][::-1])))
+            temp=np.copy(self.pattern[1][0])
+            self.pattern[1][0]=self.pattern[2][0]
+            self.pattern[2][0]=self.pattern[3][0]
+            self.pattern[3][0]=self.pattern[4][0]
+            self.pattern[4][0]=temp
+        elif face==1:
+            self.pattern[1]=np.array(list(zip(*self.pattern[1][::-1])))
+            temp=np.copy(self.pattern[0][:,0])
+            self.pattern[0][:,0]=self.pattern[4][:,1][::-1]
+            self.pattern[4][:,1]=self.pattern[5][:,0][::-1]
+            self.pattern[5][:,0]=self.pattern[2][:,0]
+            self.pattern[2][:,0]=temp
+        elif face==2:
+            self.pattern[2]=np.array(list(zip(*self.pattern[2][::-1])))
+            temp=np.copy(self.pattern[1][:,1])
+            self.pattern[1][:,1]=self.pattern[5][0]
+            self.pattern[5][0]=self.pattern[3][:,0][::-1]
+            self.pattern[3][:,0]=self.pattern[0][1]
+            self.pattern[0][1]=temp[::-1]
+        elif face==3:
+            self.pattern[3]=np.array(list(zip(*self.pattern[3][::-1])))
+            temp=np.copy(self.pattern[2][:,1])
+            self.pattern[2][:,1]=self.pattern[5][:,1]
+            self.pattern[5][:,1]=self.pattern[4][:,0][::-1]
+            self.pattern[4][:,0]=self.pattern[0][:,1][::-1]
+            self.pattern[0][:,1]=temp
+        elif face==4:
+            self.pattern[4]=np.array(list(zip(*self.pattern[4][::-1])))
+            temp=np.copy(self.pattern[3][:,1])
+            self.pattern[3][:,1]=self.pattern[5][1][::-1]
+            self.pattern[5][1]=self.pattern[1][:,0]
+            self.pattern[1][:,0]=self.pattern[0][0][::-1]
+            self.pattern[0][0]=temp
+        elif face==5:
+            self.pattern[5]=np.array(list(zip(*self.pattern[5][::-1])))
+            temp=np.copy(self.pattern[1][1])
+            self.pattern[1][1]=self.pattern[4][1]
+            self.pattern[4][1]=self.pattern[3][1]
+            self.pattern[3][1]=self.pattern[2][1]
+            self.pattern[2][1]=temp
+        else:
+            print("Not valid face number")
+            exit()
 
+    def print(self):
+        # First line
+        print('  {}{}    '.format(self.color[self.pattern[0][0][0]],self.color[self.pattern[0][0][1]]))
+        print('  {}{}    '.format(self.color[self.pattern[0][1][0]],self.color[self.pattern[0][1][1]]))
+        print('{}{}{}{}{}{}{}{}'.format(self.color[self.pattern[1][0][0]], \
+            self.color[self.pattern[1][0][1]], self.color[self.pattern[2][0][0]], \
+            self.color[self.pattern[2][0][1]], self.color[self.pattern[3][0][0]], \
+            self.color[self.pattern[3][0][1]], self.color[self.pattern[4][0][0]], \
+            self.color[self.pattern[4][0][1]]))
+        print('{}{}{}{}{}{}{}{}'.format(self.color[self.pattern[1][1][0]], \
+            self.color[self.pattern[1][1][1]], self.color[self.pattern[2][1][0]], \
+            self.color[self.pattern[2][1][1]], self.color[self.pattern[3][1][0]], \
+            self.color[self.pattern[3][1][1]], self.color[self.pattern[4][1][0]], \
+            self.color[self.pattern[4][1][1]]))
+        print('  {}{}    '.format(self.color[self.pattern[5][0][0]],self.color[self.pattern[5][0][1]]))
+        print('  {}{}    '.format(self.color[self.pattern[5][1][0]],self.color[self.pattern[5][1][1]]))
+
+    def check_face(self,face):
+        temp=self.pattern[face]
+        return np.array_equal(np.full([2,2],temp[0][0]), temp)
+
+    def check(self):
---
+++ cube_test.py
@@ -0,0 +1,4 @@
+from Cube import cube222
+# test cube functions
+mycube=cube222(0)
+mycube.print()
